2c308173226a49381820b37cd7df3af30f424387
clem9281 <clem9281@gmail.com>
2019-04-11 14:03:27 -0700

    added helper to add new user, and test for that helper

--- users/usersModel.js
+++ users/usersModel.js
@@ -1,2 +1,17 @@
 const db = require('../data/dbConfig');
 
+const find = () => db('users');
+
+const findBy = filter => db('users').where(filter);
+
+const insert = async newUser => {
+    const userId = await db('users').insert(newUser);
+    const user = await findBy({ id: userId[0] }).first();
+    return user;
+}
+
+module.exports = {
+    find,
+    findBy,
+    insert
+}
--- users/usersModel.test.js
+++ users/usersModel.test.js
@@ -1,2 +1,23 @@
-const db = require('./usersModel');
+const dbHelpers = require('./usersModel');
+const db = require('../data/dbConfig');
+const bcrypt = require('bcryptjs');
 
+describe('users model', () => {
+    // describe('find', () => {
+    //     it("should return an array of users", async () => {
+    //         const users = await dbHelpers.find();
+
+    //     })
+    // })
+    beforeEach(async () => {
+        await db('users').truncate();
+    });
+    describe('insert', () => {
+        it("should return a the created user", async () => {
+            const user = await dbHelpers.insert({ username: 'pippin', password: bcrypt.hashSync('pass', 8) })
+            expect(user).toEqual(expect.objectContaining({
+                username: 'pippin'
+            }))
+        })
+    })
+})
